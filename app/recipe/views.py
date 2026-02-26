1bcd09d0d7893e5b6fcae33e3985baf3d659c6f1
Mihir <memihirbhawsar@gmail.com>
2026-02-26 11:53:18 +0530

    work on ingredient api and testcase

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -46,4 +46,22 @@ class TagViewSet(
 
     def get_queryset(self):
         """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+
+class IngredientViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    """Manage ingredients in the databases."""
+
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter quesyset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by("-name")
